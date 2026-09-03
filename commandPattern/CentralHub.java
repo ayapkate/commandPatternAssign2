f0d0944d66840bbf453e0e535d46df7d5b7ad4e1
Kate Ayap <ayapkate.04@gmail.com>
2026-09-03 04:27:44 +0000

    Activity Files

---
+++ commandPattern/CentralHub.java
@@ -0,0 +1,13 @@
+package commandPattern;
+
+public interface CentralHub {
+    // this has the capability to command all electronic devices
+
+    public void on(); //sa music magpapeplay
+
+    public void off(); //sa music magpapause
+
+    public void increase();
+
+    public void decrease ();
+}
